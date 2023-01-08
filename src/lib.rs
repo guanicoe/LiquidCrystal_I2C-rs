6e9dbf181043d9ff572e89e7a84b2ba59a514a52
guanicoe <guanicoe@pm.me>
2023-01-08 11:48:34 +0100

    added readme with example

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,31 @@
 #![no_std]
 
+// Port of the liquide crystall I2C lirary found for arduino in rust. 
+// Tested on raspberry pi. 
+
+// Example of use:
+
+// ```rust
+// use rppal::{gpio::Gpio, i2c::I2c};
+
+// static  LCD_ADDRESS: u8 = 0x27;
+
+// fn setup() {
+
+// }
+// fn main() {
+//     let mut i2c = I2c::new().unwrap();
+//     let mut delay = rppal::hal::Delay;
+
+//     let mut lcd = screen::Lcd::new(&mut i2c, LCD_ADDRESS, &mut delay).unwrap();
+    
+//     lcd.set_display(screen::Display::On).unwrap();
+//     lcd.set_backlight(screen::Backlight::On).unwrap();
+//     lcd.print("Hello world!").unwrap();
+// }
+
+// ```
+
 use embedded_hal::blocking::{i2c, delay::DelayMs};
 
 /// Controls the visibilty of the non-blinking cursor, which is basically an _ **after** the cursor position.
